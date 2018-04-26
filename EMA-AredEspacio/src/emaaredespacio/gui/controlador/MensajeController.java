f94dddfb305e6acf7218edce92c4aeee86090135
MLJJ <acdc.ml.999@gmail.com>
2018-04-26 18:44:46 -0500

    Se modifico la presentacion de las ventanas de mensajes

--- EMA-AredEspacio/src/emaaredespacio/gui/controlador/MensajeController.java
+++ EMA-AredEspacio/src/emaaredespacio/gui/controlador/MensajeController.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package emaaredespacio.gui.controlador;
 
 
@@ -11,7 +5,6 @@ import java.util.Optional;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
-import javafx.stage.StageStyle;
 
 /**
  *
@@ -29,9 +22,8 @@ public class MensajeController {
     public static void mensajeAdvertencia(String mensaje){
         Alert ventana = new Alert(Alert.AlertType.WARNING);
         ventana.setTitle(null);
-        ventana.setHeaderText(mensaje);
-        ventana.setContentText(null);
-        ventana.initStyle(StageStyle.UNDECORATED);
+        ventana.setHeaderText(null);
+        ventana.setContentText(mensaje);
         ButtonType boton = new ButtonType("Aceptar", ButtonData.OK_DONE);
         ventana.getButtonTypes().setAll(boton);
         ventana.showAndWait();
@@ -47,7 +39,6 @@ public class MensajeController {
         ventana.setTitle(null);
         ventana.setHeaderText(null);
         ventana.setContentText(mensaje);
-        ventana.initStyle(StageStyle.UNDECORATED);
         ButtonType boton = new ButtonType("Aceptar", ButtonData.OK_DONE);
         ventana.getButtonTypes().setAll(boton);
         ventana.showAndWait();
@@ -57,20 +48,17 @@ public class MensajeController {
      * Muestra una ventana de desición.
      * 
      * @param mensaje Mensaje a mostrar.
-     * @param si Palabra 'Si' internacionalizada.
-     * @param no Palabra 'No' internacionalizada.
      * @return La desición tomada por el usuario.
      */
-    public static boolean mensajeDesicion(String mensaje, String si, String no){
+    public static boolean mensajeDesicion(String mensaje){
         boolean validacion = false;
         
         Alert ventana = new Alert(Alert.AlertType.CONFIRMATION);
         ventana.setTitle(null);
         ventana.setHeaderText(null);
         ventana.setContentText(mensaje);
-        ventana.initStyle(StageStyle.UNDECORATED);
-        ButtonType botonSi = new ButtonType(si, ButtonData.YES);
-        ButtonType botonNo = new ButtonType(no, ButtonData.NO);
+        ButtonType botonSi = new ButtonType("Si", ButtonData.YES);
+        ButtonType botonNo = new ButtonType("No", ButtonData.NO);
         ventana.getButtonTypes().setAll(botonNo,botonSi);
         
         Optional<ButtonType> resultado = ventana.showAndWait();
